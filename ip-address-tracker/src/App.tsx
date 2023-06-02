3d86811fa0cc78c4365721e8cb1d252d39e688a2
myusername <baonguyendn2711@gmail.com>
2023-06-02 16:43:17 +0700

    update code

--- ip-address-tracker/src/App.tsx
+++ ip-address-tracker/src/App.tsx
@@ -5,9 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-       <Routes>
+        <Routes>
           <Route path="/address" element={<IdAddress />} />
-          </Routes>
+        </Routes>
     </BrowserRouter>
   );
 }
