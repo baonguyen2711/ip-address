3188abb9e70ed890f425fc2e78458f3c8e29a149
myusername <baonguyendn2711@gmail.com>
2023-06-02 16:42:21 +0700

    update code

--- ip-address-tracker/src/App.tsx
+++ ip-address-tracker/src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <BrowserRouter>
        <Routes>
-                    <Route path="/address" element={<IdAddress />} />
-</Routes>
+          <Route path="/address" element={<IdAddress />} />
+          </Routes>
     </BrowserRouter>
   );
 }
